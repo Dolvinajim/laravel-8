432b1138006d68ec4a69c43ed8a05603aaad2f9f
Your Name <dolvina jim>
2020-12-19 13:51:46 +0800

    first commit

---
+++ resources/views/berita/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+<div class="content">
+    <div class="card">
+    <div class="card-body">
+    {!! Form::open(['route' => 'berita.store']) !!}
+<div class="body m-10">
+<div class="form-group">
+    {!! Form::label('judul', 'Judul'); !!}
+    {!! Form::text('judul', '', ['class' => 'form-control','placeholder'=>'berita judul']); !!}
+</div>
+<div class="form-group">
+    {!! Form::label('isi', 'Isi'); !!}
+    {!! Form::textarea('isi', '', ['class' => 'form-control','placeholder'=>'isi judul']); !!}  
+</div>
+<button class="btn btn-success">Simpan</button>
+<a href="{!! route('berita.index') !!}" class="btn btn-danger">Cancel</a>
+</div>
+
+{!! Form::close() !!}
+
+    </div>
+    </div>
+</div>
+
+
+@endsection
+
